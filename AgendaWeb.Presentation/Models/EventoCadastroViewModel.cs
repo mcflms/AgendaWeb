e9dee682268ed4fb71a4c109fd9e0f2c1fe37ee4
mcflms <mcflms@gmail.com>
2023-04-19 01:32:23 -0300

    inicio do desenvolvimento da classe EventosCadastoViewModel

---
+++ AgendaWeb.Presentation/Models/EventoCadastroViewModel.cs
@@ -0,0 +1,27 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace AgendaWeb.Presentation.Models
+{
+    /// <summary>
+    /// Classe de modelo de dados para a pagína de cadastro eventos
+    /// </summary>
+    public class EventoCadastroViewModel
+    {
+        [Required(ErrorMessage = "Por favor, informe o nome do evento.")]            
+        public string? Nome { get; set; }
+        [Required(ErrorMessage = "Por favor, informe a data do evento.")]
+        public string? Data { get; set; }
+        [Required(ErrorMessage = "Por favor, informe a hora do evento.")]
+        public string? Hora { get; set; }
+        [Required(ErrorMessage = "Por favor, informe a descrição do evento.")]
+        public string? Descricao { get; set; }
+        [Required(ErrorMessage = "Por favor, informe a prioridade do evento.")]
+        public string? Prioridade { get; set; }
+
+
+
+    }
+
+
+
+}
